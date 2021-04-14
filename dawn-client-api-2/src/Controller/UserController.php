fae8b932a200ee3c60021dcf0bfe0453d84e5753
huxiaoyan <1039368167@qq.com>
2021-04-14 21:19:49 +0800

    单点登录-多用户场景

--- dawn-client-api-2/src/Controller/UserController.php
+++ dawn-client-api-2/src/Controller/UserController.php
@@ -11,21 +11,22 @@ class UserController extends BaseController
 {
     const DAWN_TOKEN = 'dawn-sso-login-token';
 
+    const SSL_HOST = 'https://dawn.sso-api.cn';
+
     public function checkLogin(Request $request)
     {
         $cookies = $request->cookies->all();
-
         if (!isset($cookies[self::DAWN_TOKEN])) {
             return $this->fail('未登录');
         }
 
         // 去sso认证
-        $result = GuzzleHttpToolkit::post('https://dawn.sso-api.cn/check/login', ['token' => $cookies[self::DAWN_TOKEN]]);
-        if ($result['success']) {
+        $result = GuzzleHttpToolkit::post(self::SSL_HOST . '/check/login', ['token' => $cookies[self::DAWN_TOKEN]]);
+        if (true === $result['success']) {
             return $this->success($result['data'], '已登录');
         }
 
-        return $this->fail('登录认证未通过');
+        return $this->fail($result['message'], $result['data']);
     }
 
     public function login(Request $request)
@@ -38,44 +39,63 @@ class UserController extends BaseController
 
         $params = $request->request->all();
         if (empty($params['uname']) || empty($params['upwd'])) {
-            return $this->fail('参数缺失');
+            return $this->fail('参数错误');
         }
 
         // 去sso登录
-        $result = GuzzleHttpToolkit::post('https://dawn.sso-api.cn/login', $params);
+        $result = GuzzleHttpToolkit::post(self::SSL_HOST . '/login', $params);
         if (true === $result['success']) {
             return $this->success($result['data'], '登录成功');
         }
 
-        return $this->fail('登录失败');
+        return $this->fail($result['message']);
+    }
+
+    public function logout(Request $request)
+    {
+        $cookies = $request->cookies->all();
+        if (!isset($cookies[self::DAWN_TOKEN])) {
+            return $this->fail('已退出登录');
+        }
+
+        // 去sso退出登陆
+        $result = GuzzleHttpToolkit::get(self::SSL_HOST . '/logout', ['token' => $cookies[self::DAWN_TOKEN]]);
+        if (true === $result['success']) {
+            return $this->success($result['data'], '退出登陆成功');
+        }
+
+        return $this->fail($result['message']);
     }
 
     public function addCookie(Request $request)
     {
         $params = $request->query->all();
         if (empty($params['token'])) {
-            return $this->fail('参数缺失');
+            return $this->fail('参数错误');
         }
 
-        return $this->addLoginCookie($params['token']);
-    }
+        // todo：这里最好再去sso-api中验证一下token是否真的存在
 
-    protected function addLoginCookie(string $token)
-    {
-        $cookie = Cookie::create(self::DAWN_TOKEN, $token, 0, '/', null, true, true, false, 'none');
+        // 为了兼容“不兼容samesite”的浏览器而设置两种cookie
+        $cookie1 = Cookie::create(self::DAWN_TOKEN, $params['token'], 0, '/', null, true, true, false, 'none');
+        $cookie2 = Cookie::create(self::DAWN_TOKEN, $params['token'], 0, '/', null, true, true, false, null);
 
         $response = new Response();
-        $response->headers->setCookie($cookie);
+        $response->headers->setCookie($cookie2);
+        $response->headers->setCookie($cookie1);
 
         return $response;
     }
 
-    public function logout()
+    public function clearCookie()
     {
-        $cookie = Cookie::create(self::DAWN_TOKEN, null, 0, '/', null, true, true, false, 'none');
+        // 为了兼容“不兼容samesite”的浏览器而设置两种cookie
+        $cookie2 = Cookie::create(self::DAWN_TOKEN, null, 0, '/', null, true, true, false, null);
+        $cookie1 = Cookie::create(self::DAWN_TOKEN, null, 0, '/', null, true, true, false, 'none');
 
         $response = new Response();
-        $response->headers->setCookie($cookie);
+        $response->headers->setCookie($cookie2);
+        $response->headers->setCookie($cookie1);
 
         return $response;
     }
--- dawn-sso-api/src/Controller/UserController.php
+++ dawn-sso-api/src/Controller/UserController.php
@@ -4,15 +4,23 @@ namespace App\Controller;
 
 use App\Common\RandcharToolkit;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Firebase\JWT\JWT;
 
 class UserController extends BaseController
 {
-    const USER_FILE = '/dawn-sso/dawn-sso-api/var/user.txt'; // 该地址根据本地文件夹情况自定义
+    const USER_LIST = [
+        ['uname' => '1', 'upwd' => '1', 'uid' => '100001', 'urealname' => '胡晓晓1', 'ugender' => '女'],
+        ['uname' => '2', 'upwd' => '2', 'uid' => '100002', 'urealname' => '胡晓晓2', 'ugender' => '男'],
+    ];
+
+    const JWT_SECRET_KEY = 'dqtBcibCiQYuOLq3';
 
     const DOMAIN_1 = 'https://dawn.sso-client-1.cn';  // 客户端1地址
     const DOMAIN_2 = 'https://dawn.sso-client-2.cn';  // 客户端2地址
 
+    const TOKEN_FILE_DIR = '/Users/mac/Codes/dawn-sso/dawn-sso-api/var/';
+
+    // 用于客户端请求验证是否登陆
     public function checkLogin(Request $request)
     {
         $params = $request->request->all();
@@ -20,14 +28,21 @@ class UserController extends BaseController
             return $this->fail('参数缺失');
         }
 
-        $token = trim(\file_get_contents(self::USER_FILE));
-        if ($token === $params['token']) {
-            return $this->success(['uname' => '胡晓晓', 'ugender' => '女'], '已登录');
+        $fPath = self::TOKEN_FILE_DIR . $params['token'] . '.txt';
+        if (!\file_exists($fPath)) {
+            return $this->fail('已经退出登陆', ['logout' => true]);
         }
 
-        return $this->fail('认证错误');
+        $fInfos = \json_decode(\file_get_contents($fPath), true);
+        $tInfos = (array) JWT::decode($params['token'], self::JWT_SECRET_KEY, ['HS256']);
+        if ($fInfos['ticket'] !== $tInfos['ticket'] || $fInfos['timestamp'] !== $tInfos['timestamp'] || $fInfos['signer'] !== $tInfos['signer']) {
+            return $this->fail('认证失败');
+        }
+
+        return $this->success(['uid' => $fInfos['uid'], 'urealname' => $fInfos['urealname'], 'ugender' => $fInfos['ugender']], '已登录');
     }
 
+    // 用于客户端请求登陆验证
     public function login(Request $request)
     {
         $params = $request->request->all();
@@ -35,29 +50,59 @@ class UserController extends BaseController
             return $this->fail('参数缺失');
         }
 
-        if ('1' === $params['uname'] && '1' === $params['upwd']) {
-            $token = RandcharToolkit::genChars(32, 7);
-            \file_put_contents(self::USER_FILE, $token);
-
-            return $this->success([
-                'domainList' => [self::DOMAIN_1, self::DOMAIN_2],
-                'token' => $token,
-            ], '登录成功');
+        $isAuth = false;
+        $user = [];
+        foreach (self::USER_LIST as $uitem) {
+            if ($uitem['uname'] === $params['uname'] && $uitem['upwd'] === $params['upwd']) {
+                $isAuth = true;
+                $user = $uitem;
+                break;
+            }
         }
 
-        return $this->fail('登录失败');
+        if (!$isAuth) {
+            return $this->fail('登录失败');
+        }
+
+        $ticket = RandcharToolkit::genChars(32, 7);
+
+        $nTime = time();
+        $info = [
+            'ticket' => $ticket,
+            'timestamp' => $nTime,
+            'signer' => md5($ticket . $nTime . self::JWT_SECRET_KEY),
+        ];
+
+        $token = JWT::encode($info, self::JWT_SECRET_KEY);
+
+        $info['token'] = $token;
+        $info['uid'] = $user['uid'];
+        $info['urealname'] = $user['urealname'];
+        $info['ugender'] = $user['ugender'];
+
+        \file_put_contents(self::TOKEN_FILE_DIR . $token . '.txt', \json_encode($info, \JSON_UNESCAPED_UNICODE));
+
+        return $this->success([
+            'domainList' => [self::DOMAIN_1, self::DOMAIN_2],
+            'token' => $token,
+        ], '登录成功');
     }
 
+    // 用于客户端请求登出验证
     public function logout(Request $request)
     {
         $params = $request->query->all();
+        if (empty($params['token'])) {
+            return $this->fail('参数缺失');
+        }
 
-        \file_put_contents(self::USER_FILE, '');
+        $fPath = self::TOKEN_FILE_DIR . $params['token'] . '.txt';
+        if (!\file_exists($fPath)) {
+            return $this->success(['domainList' => [self::DOMAIN_1, self::DOMAIN_2], 'logout' => true], '已经退出登陆');
+        }
 
-        $domainList = \json_encode([self::DOMAIN_1, self::DOMAIN_2], \JSON_UNESCAPED_UNICODE);
+        @\unlink($fPath);
 
-        $response = new Response($params['callback'] . '(' . $domainList . ')');
-
-        return $response;
+        return $this->success(['domainList' => [self::DOMAIN_1, self::DOMAIN_2], 'logout' => true], '已经退出登陆');
     }
 }
